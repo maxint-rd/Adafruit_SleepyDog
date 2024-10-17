d8dcb3f9ede38ac6e627085ade73d0c056f57cef
Maxint R&D <mm_rd@maxint.nl>
2024-10-17 11:14:52 +0200

    Added CH32X035 / CH32X033

--- utility/WatchdogCH32.cpp
+++ utility/WatchdogCH32.cpp
@@ -38,12 +38,44 @@ void WatchdogCH32::gpios_off() {
 }
 */
 
+void WatchdogCH32::EXTI_INT_INIT(void)
+{   // copied from https://github.com/openwch/ch32x035/blob/main/EVT/EXAM/PWR/Standby_Mode/User/main.c
+    EXTI_InitTypeDef EXTI_InitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+    EXTI_InitStructure.EXTI_Line = EXTI_Line27;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+}
+
 extern "C" {
 extern __IO uint64_t msTick;      // the msTick counter will be updated after sleeping
 }
 
+
+#if defined(CH32X035)
+#define OPT_X035_USE_INTERRUPT 0
+#if (OPT_X035_USE_INTERRUPT)
+extern "C" {
+void AWU_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void AWU_IRQHandler()
+{
+    EXTI_ClearITPendingBit(EXTI_Line27);
+    SystemInit();
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, DISABLE);  // SDI=system debug interface
+    //Serial.printf("\r\n Auto wake up \r\n");
+}
+} // extern "C"
+#endif // #if defined(OPT_X035_USE_INTERRUPT)
+#endif // #if defined(CH32X035)
+
 uint32_t WatchdogCH32::pwr_sleep(uint32_t uSleepMS)
 { // Sleep for some time, then wake up automatically
+  // The CH32V003 has a Low Speed clock (running at 128kHz), that can be used to wakeup from standby.
   // The longest single sleep amount is approx 30 seconds (as tested on CH32V003). 
   // To reach the requested amount of sleep, repeated naps are done until the requested amount is reached
   // WARNING: You MUST power cycle the CH32V003 to allow it to go into deep sleep. After reset by flashing or by Watchdog sleep is NOY enabled!
@@ -65,6 +97,15 @@ uint32_t WatchdogCH32::pwr_sleep(uint32_t uSleepMS)
       PWR_AWU_SetWindowValue(25);
       PWR_AutoWakeUpCmd(ENABLE);
       PWR_EnterSTANDBYMode(PWR_STANDBYEntry_WFE);
+      
+   CH32X035/X033 has no LSI clock. To wakeup 47KHz divided clock of the internal high-speed clock HSI as the AWU module clock source:
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
+   To wake up from low power mode, the external interrupt line 27 needs to be configured as a rising edge interrupt.
+   See also AWU in the EVT Standby example: https://github.com/openwch/ch32x035/blob/main/EVT/EXAM/PWR/Standby_Mode/User/main.c:
+      AWU_SetPrescaler(AWU_Prescaler_10240);
+      AWU_SetWindowValue(25);  // 25/(48M/1024/10240) = 5.46s
+      AutoWakeUpCmd(ENABLE);
+      PWR_EnterSTANDBYMode();
 */
 
   // calculate sleep settings and repeat until all sleep is done
@@ -73,38 +114,73 @@ uint32_t WatchdogCH32::pwr_sleep(uint32_t uSleepMS)
   //uint32_t nSleep=128000/61440;   // 128000/61440=2.08333, 63/128000/61440=30,240
   uint32_t uPrescaler=PWR_AWU_Prescaler_61440;
   uint32_t uWindow=63;
+//Serial.print("z");
   
   // Nap repeatedly, until indicated sleep time has passed
-  while(nSleepLeft>500)   // PWR_AWU_Prescaler_61440 allows for 0.48s resolution
+  while(nSleepLeft>500)   // PWR_AWU_Prescaler_61440 allows for 0.48s resolution on V003
   {
+//Serial.print("0");
     // Determine prescaler and window values needed for largest nap we can do
     // TODO: See if other prescaler provides better fit. For short durations a smaller prescaler is more precise.
-    // Highest prescaler PWR_AWU_Prescaler_61440 allows for 0.48-30.24 seconds sleep
-    // Prescaler_10240 offers finer resolution for up to 0.08-5.04 seconds sleep
-    // 128000/61440=2.08333, 63/128000/61440=30.240
-    // 128000/10240=12.5, 63/128000/10240=5.040
+    // V003: Highest prescaler PWR_AWU_Prescaler_61440 allows for 0.48 up to 30.24 seconds sleep
+    //       Prescaler_10240 offers finer resolution for 0.08 up to 5.04 seconds sleep
+    //       128000/61440=2.08333, 63/128000/61440=30.240
+    //       128000/10240=12.5, 63/128000/10240=5.040
+    // X035: HSI 48MHz clock is used with a divider of 1024, giving a 46.875kHz divided clock.
+    //       Highest prescaler AWU_Prescaler_61440 gives longggg sleep (82,6 sec) at terrible resolution
+    //       Prescaler_10240 offers finer resolution for 0.22 up to 13.76 seconds sleep
+    //       48000000/1024/61440=0,762939453125 => 1,31072 sec/count (max 82,57536 sec)
+    //       48000000/1024/10240=4,57763671875 => 0,21845333 sec/count (max 13,76256 sec)
+#if defined(CH32V00x)
+    #define AWU_DIVIDED_COUNTS_PER_KSEC 2083   // 128000/61440=2.08333 => 0.48 sec/count
+#elif defined(CH32X035)
+    uPrescaler=PWR_AWU_Prescaler_10240;
+    #define AWU_DIVIDED_COUNTS_PER_KSEC 4578  // 48000000/1024/10240=4,57763671875 => 0,21845333 sec/count (max 13,76256 sec)
+#endif
 
     // find the window value that best matches sleep time left
-    uWindow=63;
-    while((uWindow*1000000L)/2083>nSleepLeft && uWindow>1)
+    uWindow=63;  // IWDG window downcounter must be lower than 0x3F=63d
+    while((uWindow*1000000L)/AWU_DIVIDED_COUNTS_PER_KSEC>nSleepLeft && uWindow>1)
       uWindow--;
 
     // calculate actual sleep time for window value
-    uint32_t nSleep=(uWindow*1000000L)/2083;
+    uint32_t nSleep=(uWindow*1000000L)/AWU_DIVIDED_COUNTS_PER_KSEC;
     nSleepDone+=nSleep;
     nSleepLeft-=nSleep;
 
     // enable power interface module clock
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
 
-    // TODO: I saw no function that can be called to enable the AutoWakeUp event. Perhaps it is EXTI_Init()?
+    // TODO: I saw no function that can be called to enable the AutoWakeUp event. WCH Examples use EXTI_Init().
     // Only CH32V003 uses EXTI_Line9 for the wake up event. Other family members use other lines (eg. CH32X035 uses EXTI_Line27)
-    // Unfortunately I don't have other CH32 chips to test and add support.
+    // Unfortunately I don't have all other CH32 chips to test and add support.
     // As far as I can see the current core (1.0.4) has no common function to select the line used for AutoWakeUp.
     // I suggest to make this all part of some function related to PWR_AutoWakeUpCmd()
-    // enable AutoWakeUp event  EXTI_Init( EXTI_Line9 EXTI_Mode_Event EXTI_Trigger_Falling ENABLE)
+    // V003: enable AutoWakeUp event  EXTI_Init( EXTI_Line9 EXTI_Mode_Event EXTI_Trigger_Falling ENABLE)
+    // X035: enable AutoWakeUp event  EXTI_Init( EXTI_Line27 EXTI_Mode_Interrupt EXTI_Trigger_Falling ENABLE)
+#if defined(CH32V00x)
     EXTI->EVENR |= EXTI_Line9;
     EXTI->FTENR |= EXTI_Line9;
+#elif defined(CH32X035)
+  //RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+    //RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE); // when allowing pin wakeup
+
+    //EXTI->EVENR |= EXTI_Line27;
+    EXTI->INTENR |= EXTI_Line27;      // no event, only intterrupt
+    EXTI->FTENR |= EXTI_Line27;
+    //EXTI_INT_INIT();
+
+  #if (OPT_X035_USE_INTERRUPT)
+    NVIC_InitTypeDef NVIC_InitStructure = {0};
+    NVIC_InitStructure.NVIC_IRQChannel = AWU_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+  #endif
+#else
+    //#warning(NOTE: no support for other CH32 mcu) 
+#endif
 
     // configure AWU prescaler
     PWR_AWU_SetPrescaler(uPrescaler); // possible values: 1/2/4/8/.../4096, 10240, 61440
@@ -112,19 +188,31 @@ uint32_t WatchdogCH32::pwr_sleep(uint32_t uSleepMS)
     // configure AWU window comparison value
     PWR_AWU_SetWindowValue(uWindow);
 
+#if defined(CH32V00x)
     // enable low speed oscillator (LSI)
     RCC_LSICmd(ENABLE);
     while(RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET);
+#endif
 
     // enable AWU
     PWR_AutoWakeUpCmd(ENABLE);
 
     // Select deep sleep on power-down (PWR_CTLR_PDDS) and use WFE command to enter Sleep mode.
-    PWR_EnterSTANDBYMode(PWR_STANDBYEntry_WFE);       // PWR_STANDBYEntry_WFI is wake up by interrupt, _WFE is wake up by event
-    
+    PWR_EnterSTANDBYMode(PWR_STANDBYEntry_WFE);       // PWR_STANDBYEntry_WFI is wake up by interrupt, _WFE is wake up by event, 
+                                                      // _WFE only available on V003/VM00x, X035 always calls __WFI()=Wait for Interrupt
+#if defined(CH32V00x)
     // Back from sleep; restore clock to full speed
     SystemInit();
+#elif defined(CH32X035)
+#if !(OPT_X035_USE_INTERRUPT)
+    EXTI_ClearITPendingBit(EXTI_Line27);  // clear the pending bit so we can sleep again later
+    SystemInit();
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, DISABLE);  // SDI=system debug interface
+#endif
+#endif
   }
+
   // Update millis() with approximation of time spend sleeping...
   msTick+=nSleepDone;     // TODO: More accurate measument of sleep done. Perhaps we could use millis() to measure time for calls above
   return(nSleepDone);
@@ -141,6 +229,7 @@ uint32_t WatchdogCH32::pwr_sleep(uint32_t uSleepMS)
 int WatchdogCH32::enable(int maxPeriodMS) {
   if (maxPeriodMS < 0)
     return 0;
+  //_wdto = maxPeriodMS;
 
   // Enables the CH32's hardware WDT with maxPeriodMS delay
   // (wdt should be updated every maxPeriodMS ms) and
@@ -153,17 +242,45 @@ int WatchdogCH32::enable(int maxPeriodMS) {
   // TODO: find best values for prescaler and counter to match maxPeriodMS
   //       Return value actually used (in ms)
 
-  // The independent watchdog uses the LSI clock which runs at 128kHz, using the IWDG_Prescaler_128, the 12 bit counter allows up to 4096 msec
+  // On the CH32V003 the independent watchdog uses the LSI clock which runs at 128kHz, using the IWDG_Prescaler_128, the 12 bit counter allows up to 4096 msec
+  // On the CH32X035/X033, the 48000MHz HSI clock is used with a 1024 divider. 48MHz/1024=46.875kHz. Using (IWDG_Prescaler_32, 4000) gives 2.7s IWDG reset
 	// set up watchdog (0xfff=4096d, with prescaler 128 this is about 4sec on the CH32V003)
+#if defined(CH32V003)
   uint8_t prescaler=IWDG_Prescaler_128;
-  if(maxPeriodMS<=0xfff)
-    iwdg_setup(maxPeriodMS, prescaler);  // set up watchdog
+  #define PERIOD_FIX(x) (x)
+  #define PERIOD_FIX_REVERSE(x) (x)
+#endif
+#if defined(CH32X035)
+  uint8_t prescaler=IWDG_Prescaler_32;
+  // transform maxPeriodMS to comply to 46.875kHz clock and smaller prescaler. 
+  //#define PERIOD_FIX(x)  ((uint16_t)(((uint32_t)(x)*4L*46875L)/128000L))    // 46875L/128000L=0,3662109375
+  //#define PERIOD_FIX_REVERSE(x)  ((uint16_t)(((uint32_t)(x)*128000L)/(4L*46875L)))    // 128000L/46875L=2.73066667
+  #define PERIOD_FIX(x)  ((uint16_t)(((uint32_t)(x)*1875L)/1280L))    // 4L*46875L=187500L, 46875L/128000L=0,3662109375
+  #define PERIOD_FIX_REVERSE(x)  ((uint16_t)(((uint32_t)(x)*1280L)/1875L))    // 4L*46875L=187500L,  128000L/46875L=2.73066667
+  //Serial.printf("\nMAXP:%d\n", PERIOD_FIX(maxPeriodMS));
+#endif
+  if(PERIOD_FIX(maxPeriodMS)<=0xfff)    // < 4096
+    iwdg_setup(PERIOD_FIX(maxPeriodMS), prescaler);  // set up watchdog
   else
-  {   // CH32 supports prescaler up to 256, allowing for max 8192 mSec timout. Since a different clock is used that duration may be not very precise
+  {   // CH32 supports prescaler up to 256, allowing for max 8192 mSec timout on V003. Since a different clock is used that duration may be not very precise
+      // On the X035/X035 the HSI is used with a 1024 devider. Using the 256 prescaler gives a maximum timeout of 22.3 sec.
+#if defined(CH32V003)
     prescaler=IWDG_Prescaler_256;
     if(maxPeriodMS>0x1fff)
       maxPeriodMS=0x1fff;
     iwdg_setup(maxPeriodMS/2, prescaler);  // set up watchdog
+#endif
+#if defined(CH32X035)
+    prescaler=IWDG_Prescaler_256; // use largest prescaler
+    //if((uint16_t)(PERIOD_FIX(maxPeriodMS)/8L)>(uint16_t)0xfff)
+    //  maxPeriodMS=(uint16_t)(PERIOD_FIX_REVERSE((uint16_t)0xfff*8L));
+    if(maxPeriodMS>22364)   // maximum 22364ms found using PERIOD_FIX_REVERSE
+      maxPeriodMS=22364;
+    iwdg_setup(PERIOD_FIX(maxPeriodMS)/8L, prescaler);  // using largest prescaler still requires reload_val to be <0x1000 (4096d)
+#endif
+//    if(PERIOD_FIX(maxPeriodMS)>0x1fff)
+//      maxPeriodMS=PERIOD_FIX_REVERSE(0x1fff);
+//    iwdg_setup(PERIOD_FIX(maxPeriodMS/2), prescaler);  // set up watchdog
   }
 
  /*
--- utility/WatchdogCH32.h
+++ utility/WatchdogCH32.h
@@ -20,6 +20,16 @@
 
 //#include <hardware/watchdog.h>
 
+// fix some CH32 core differences
+#if defined(CH32X035)     // the CH32X035 define is also used for X033
+    #define PWR_AWU_Prescaler_61440 AWU_Prescaler_61440
+    #define PWR_AWU_Prescaler_10240 AWU_Prescaler_10240
+    #define PWR_AWU_SetPrescaler AWU_SetPrescaler           // AWU_SetPrescaler only for X035/X033, PWR_AWU_SetPrescaler only for V003,VM00x
+    #define PWR_AutoWakeUpCmd AutoWakeUpCmd // AutoWakeUpCmd only for X035/X033,  PWR_AutoWakeUpCmd only for V003,VM00x
+    #define PWR_AWU_SetWindowValue AWU_SetWindowValue // AWU_SetWindowValue only for X035/X033,  PWR_AWU_SetWindowValue only for V003,VM00x
+    #define PWR_STANDBYEntry_WFE    // no different standby entries for CH32X035/X033: void PWR_EnterSTANDBYMode(void)
+#endif
+
 /**************************************************************************/
 /*!
     @brief  Class that contains functions for interacting with the
@@ -28,14 +38,14 @@
 /**************************************************************************/
 class WatchdogCH32 {
 public:
-  //WatchdogCH32() : _wdto(-1){};
+  //WatchdogCH32() : _wdto(-1){};  // constuctor init private vars
   int enable(int maxPeriodMS = 0);  // enable watchdog
   void disable()      __attribute__((error("CH32 IWDG cannot be disabled!")));
   void reset();   // feed the dog
-#if defined(CH32V00x)
+#if defined(CH32V00x) || defined(CH32X035)
   int sleep(int maxSleepMS = 30000);
 #else
-  int sleep(int maxSleepMS = 0) __attribute__((error("CH32 sleep() only implemented for CH32V00x! (for now?)")));
+  int sleep(int maxSleepMS = 0) __attribute__((error("CH32 sleep() only implemented for CH32V00x/X033/X035! (for now?)")));
 #endif
 
 private:
@@ -45,6 +55,7 @@ private:
   uint32_t pwr_sleep(uint32_t uSleep);
   void gpios_on();
   void gpios_off();
+  void EXTI_INT_INIT();
 
 
 
